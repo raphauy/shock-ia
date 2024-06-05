dae661a97c81cad9f0f940e22cc1d5f8ab1d18ed
raphauy <rapha.uy@rapha.uy>
2024-06-05 19:27:36 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        outputFileTracingIncludes: {
+          "/api/chat": ["./node_modules/tiktoken/tiktoken_bg.wasm"],
+        },
+    },
 }
 
 module.exports = nextConfig
