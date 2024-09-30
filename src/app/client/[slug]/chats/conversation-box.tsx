92afd25aa149d7a12b90fd8603b050b190c6c8cb
raphauy <rapha.uy@rapha.uy>
2024-09-30 14:42:36 -0300

    small UI improve

--- src/app/client/[slug]/chats/conversation-box.tsx
+++ src/app/client/[slug]/chats/conversation-box.tsx
@@ -1,4 +1,5 @@
 
+import React from "react"
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
@@ -53,63 +54,66 @@ export default function ConversationBox({ conversation, promptTokensPrice, compl
   
 
   return (
-      <main className="flex flex-col items-center justify-between w-full p-3 border-l">
-        <div className="flex justify-between w-full pb-2 text-center border-b">
-          <div className="place-self-end">
-            {customInfo?.summitId &&
-              <Link href={`/client/summit/summit?summitId=${customInfo.summitId}`}>
-                <Button variant="ghost" className="h-7"><Ticket /></Button>
-              </Link>
-            }
-            {customInfo?.carServiceName &&
-              <Link href={`/client/${slug}/car-service?name=${customInfo.carServiceName}`}>
-                <Button variant="ghost" className="h-7"><Car /></Button>
-              </Link>
-            }
-          </div>
-          <div>
-            <p className="text-lg font-bold">{conversation.celular} ({conversation.fecha})</p>
-            {
-              totalPromptTokens > 0 && isAdmin && (
-                <div className="flex items-center justify-center gap-2">
-                  <p>{Intl.NumberFormat("es-UY").format(totalPromptTokens)} pt</p>
-                  <p>+</p>
-                  <p>{Intl.NumberFormat("es-UY").format(totalCompletionTokens)} ct</p>
-                  <p>=</p>
-                  <p>{Intl.NumberFormat("es-UY").format(totalPromptTokens + totalCompletionTokens)} tokens</p>
-                  <Separator orientation="vertical" className="h-6 mx-1" />
-                  <CircleDollarSign size={18} />
-                  <p>{Intl.NumberFormat("es-UY").format(promptTokensValue + completionTokensValue)} USD</p>
-                </div>                  
-              )
-            }
-          </div>
-          <div className="flex items-center gap-2">
-            {isAdmin && 
-              <>
-                <p>Prompt:</p><Switch checked={showSystem} onCheckedChange={setShowSystem} />
-                <DeleteConversationDialog id={conversation.id} description={`Seguro que desea eliminar la conversación de ${conversation.celular}`} redirectUri={`${conversation.clienteSlug}`} />
-              </>
-            }
-          </div>          
-        </div>  
+    <main className="flex flex-col items-center justify-between w-full p-3 border-l">
+      <div className="flex justify-between w-full pb-2 text-center border-b">
+        <div className="place-self-end">
+          {customInfo?.summitId &&
+            <Link href={`/client/summit/summit?summitId=${customInfo.summitId}`}>
+              <Button variant="ghost" className="h-7"><Ticket /></Button>
+            </Link>
+          }
+          {customInfo?.carServiceName &&
+            <Link href={`/client/${slug}/car-service?name=${customInfo.carServiceName}`}>
+              <Button variant="ghost" className="h-7"><Car /></Button>
+            </Link>
+          }
+        </div>
+        <div>
+          <p className="text-lg font-bold">{conversation.celular} ({conversation.fecha})</p>
+          {
+            totalPromptTokens > 0 && isAdmin && (
+              <div className="flex items-center justify-center gap-2">
+                <p>{Intl.NumberFormat("es-UY").format(totalPromptTokens)} pt</p>
+                <p>+</p>
+                <p>{Intl.NumberFormat("es-UY").format(totalCompletionTokens)} ct</p>
+                <p>=</p>
+                <p>{Intl.NumberFormat("es-UY").format(totalPromptTokens + totalCompletionTokens)} tokens</p>
+                <Separator orientation="vertical" className="h-6 mx-1" />
+                <CircleDollarSign size={18} />
+                <p>{Intl.NumberFormat("es-UY").format(promptTokensValue + completionTokensValue)} USD</p>
+              </div>                  
+            )
+          }
+        </div>
+        <div className="flex items-center gap-2">
+          {isAdmin && 
+            <>
+              <p>Prompt:</p><Switch checked={showSystem} onCheckedChange={setShowSystem} />
+              <DeleteConversationDialog id={conversation.id} description={`Seguro que desea eliminar la conversación de ${conversation.celular}`} redirectUri={`${conversation.clienteSlug}`} />
+            </>
+          }
+        </div>          
+      </div>  
 
-        <div className="w-full max-w-3xl mt-5 ">
-        {
-          messages.map((message, i) => (
-            <div key={i} className="w-full">
-              <div className={clsx(
-                  "flex w-full items-center justify-between px-1 lg:px-4 border-b border-gray-200 py-5",
-                  message.role === "user" ? "bg-gray-100" : "bg-white",
-                )}
-              >
-                <div className="flex items-center w-full max-w-screen-md px-5 space-x-4 sm:px-0">
+      <div className="w-full max-w-3xl mt-5 ">
+      {
+        messages.map((message, i) => (
+          <div key={i} className="w-full">
+            <div className={clsx(
+                "flex w-full items-center justify-between px-1 lg:px-4 border-b border-gray-200 py-5",
+                message.role === "user" ? "bg-gray-100 dark:bg-gray-800" : "bg-background",
+              )}
+            >
+              <div className="flex items-center w-full max-w-screen-md px-5 space-x-4 sm:px-0">
+                {
+                  // @ts-ignore
+                  !message.gptData &&
                   <div className="flex flex-col">
                     <div
                         className={clsx(
                         "p-1.5 text-white flex justify-center",
-                        (message.role === "assistant" || message.role === "function") ? "bg-green-500" : message.role === "system" ? "bg-blue-500" : "bg-black",
-                        )}
+                        (message.role === "assistant") ? "bg-green-500" : (message.role === "system" || message.role === "function") ? "bg-blue-500" : "bg-black",
+                      )}
                     >
                         {message.role === "user" ? (
                         <User width={20} />
@@ -122,20 +126,23 @@ export default function ConversationBox({ conversation, promptTokensPrice, compl
                     </div>
                     <p className="text-sm">{message.fecha}</p>
                   </div>
-                  {message.role === "system" ?
-                  <Accordion type="single" collapsible className="w-full">
-                    <AccordionItem value="Prompt">
-                      <AccordionTrigger>Prompt</AccordionTrigger>
-                      <AccordionContent>
-                        <div className="whitespace-pre-line">
-                          {removeSectionTexts(message.content)}
-                        </div>
-                      </AccordionContent>
-                    </AccordionItem>
-                  </Accordion> :
-                    <div className="w-full">
+                }
+                {message.role === "system" ?
+                <Accordion type="single" collapsible className="w-full">
+                  <AccordionItem value="Prompt">
+                    <AccordionTrigger>Prompt</AccordionTrigger>
+                    <AccordionContent>
+                      <div className="whitespace-pre-line">
+                        {removeSectionTexts(message.content)}
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion> :
+                  <div className="w-full">
+                    {
+                      message.role != "system" && message.role != "function" &&
                       <ReactMarkdown                        
-                        className="prose break-words prose-p:leading-relaxed"
+                        className="prose break-words prose-p:leading-relaxed dark:prose-invert"
                         remarkPlugins={[remarkGfm]}
                         components={{
                           // open links in new tab
@@ -146,31 +153,32 @@ export default function ConversationBox({ conversation, promptTokensPrice, compl
                       >
                         {message.content}
                       </ReactMarkdown>
-                    </div>
-                }
-                </div>
-                {
-                  message.promptTokens > 0 && isAdmin ? (
-                    <div className="grid p-2 text-right border rounded-md">
-                      <p className="whitespace-nowrap">{Intl.NumberFormat("es-UY").format(message.promptTokens)} pt</p>
-                      <p>{Intl.NumberFormat("es-UY").format(message.completionTokens)} ct</p>
-                    </div>
-                  ) : 
-                  <div></div>
-                }
+                    }
+                  </div>
+              }
               </div>
               {
-                message.gptData && isAdmin && (
-                  <GPTData gptData={message.gptData} slug={conversation.clienteSlug} />
-                )
-              }              
+                message.promptTokens > 0 && isAdmin ? (
+                  <div className="grid p-2 text-right border rounded-md">
+                    <p className="whitespace-nowrap">{Intl.NumberFormat("es-UY").format(message.promptTokens)} pt</p>
+                    <p>{Intl.NumberFormat("es-UY").format(message.completionTokens)} ct</p>
+                  </div>
+                ) : 
+                <div></div>
+              }
             </div>
-          ))
-        }
-        </div>
+            {/* {
+              message.gptData && isAdmin && (
+                <GPTData gptData={message.gptData} slug={conversation.clienteSlug} />
+              )
+            }               */}
+          </div>
+        ))
+      }
+      </div>
 
-      </main>
-    );
-  }
+    </main>
+  );
+}
 
 
