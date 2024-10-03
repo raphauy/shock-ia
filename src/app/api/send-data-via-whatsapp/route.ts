92d119cae918d46586fd6477199c84c736ac8caf
raphauy <rapha.uy@rapha.uy>
2024-10-03 12:51:41 +0000

    conv url on whatsapp

--- src/app/api/send-data-via-whatsapp/route.ts
+++ src/app/api/send-data-via-whatsapp/route.ts
@@ -45,7 +45,7 @@ export async function POST(request: Request) {
             text += `**${keyWithTildes}**: ${value}\n`
         }
         text+= "------------------------------------\n"
-        text+= `[Ver conversación](${conversationUrl})\n`
+        text+= `${conversationUrl}\n`
         console.log("text: ", text)
         
 
