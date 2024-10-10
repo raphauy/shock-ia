139733863d399eaab012481795d59b2aee63c3c4
raphauy <rapha.uy@rapha.uy>
2024-10-10 08:18:18 -0300

    Fixed date event type

--- src/app/admin/clients/(crud)/client-dialog.tsx
+++ src/app/admin/clients/(crud)/client-dialog.tsx
@@ -169,7 +169,8 @@ export function ClientFunctionsBox({ clientId, closeDialog }: ClientFunctionBoxP
                     // obtenerDisponibilidad
                     // reservarParaEvento
                     // obtenerReservas
-                    const isEventosFunction= item.name === "cancelarReserva" || item.name === "obtenerDisponibilidad" || item.name === "reservarParaEvento" || item.name === "obtenerReservas"
+                    // reservarParaEventoDeUnicaVez
+                    const isEventosFunction= item.name === "cancelarReserva" || item.name === "obtenerDisponibilidad" || item.name === "reservarParaEvento" || item.name === "obtenerReservas" || item.name === "reservarParaEventoDeUnicaVez"
                     return (
                         <div key={item.id} className="flex items-center justify-between gap-2 mb-1 mr-5">
                             <p className="text-green-500 whitespace-nowrap">{item.name}</p>
@@ -187,7 +188,7 @@ export function ClientFunctionsBox({ clientId, closeDialog }: ClientFunctionBoxP
               <div className="flex flex-col">
               {
                   complementary.map((item) => {
-                    const isEventosFunction= item.name === "cancelarReserva" || item.name === "obtenerDisponibilidad" || item.name === "reservarParaEvento" || item.name === "obtenerReservas"
+                    const isEventosFunction= item.name === "cancelarReserva" || item.name === "obtenerDisponibilidad" || item.name === "reservarParaEvento" || item.name === "obtenerReservas" || item.name === "reservarParaEventoDeUnicaVez"
                     return (
                         <div key={item.id} className="flex items-center gap-2 mb-1">
                             <Button variant="secondary" className="h-7 x-7" onClick={() => complementaryIn(item.id)} disabled={isEventosFunction}>
