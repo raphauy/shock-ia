9d087b27072e2849c90f039b33c52801b7c04a4d
raphauy <rapha.uy@rapha.uy>
2024-11-06 10:35:57 -0300

    crm 2024_11_6

--- src/app/client/[slug]/crm/kanban/contact-card.tsx
+++ src/app/client/[slug]/crm/kanban/contact-card.tsx
@@ -18,30 +18,32 @@ export default function ContactCard({ contact, index }: Props) {
     return (
         <Draggable draggableId={contact.id} index={index}>
             {(provided) => (
-                <Card {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                  <CardContent className="p-3">
-                    <div className="flex justify-between items-center">
-                      <div className="flex items-center space-x-4">
-                        <Avatar>
-                          <AvatarImage src={contact.imageUrl ?? ''} alt={contact.name} />
-                          <AvatarFallback>{contact.name.charAt(0)}</AvatarFallback>
-                        </Avatar>
-                        <div>
-                          <h3 className="font-medium">{contact.name}</h3>
-                          <p className="text-sm text-gray-500">{contact.phone}</p>
+                <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                  <Card>
+                    <CardContent className="p-3">
+                      <div className="flex justify-between items-center">
+                        <div className="flex items-center space-x-4">
+                          <Avatar>
+                            <AvatarImage src={contact.imageUrl ?? ''} alt={contact.name} />
+                            <AvatarFallback>{contact.name.charAt(0)}</AvatarFallback>
+                          </Avatar>
+                          <div>
+                            <h3 className="font-medium">{contact.name}</h3>
+                            <p className="text-sm text-gray-500">{contact.phone}</p>
+                          </div>
                         </div>
+                        <Button variant="outline" size="icon" className="size-8">
+                          <Expand className="h-4 w-4" />
+                        </Button>
                       </div>
-                      <Button variant="outline" size="icon" className="size-8">
-                        <Expand className="h-4 w-4" />
-                      </Button>
-                    </div>
-                    <div className={cn("mt-3 flex flex-wrap justify-center gap-1", contact.tags.length === 0 && "hidden")}>
-                      {contact.tags.map((tag, index) => (
-                        <Badge key={index} variant="secondaryWithBorder">{tag}</Badge>
-                      ))}
-                    </div>
-                  </CardContent>
-                </Card>
+                      <div className={cn("mt-3 flex flex-wrap justify-center gap-1", contact.tags.length === 0 && "hidden")}>
+                        {contact.tags.map((tag, index) => (
+                          <Badge key={index} variant="secondaryWithBorder">{tag}</Badge>
+                        ))}
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
             )}
         </Draggable>
     )
