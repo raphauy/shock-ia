70d344af3ee89a29e496d37dc14ff7a09bf64b35
raphauy <rapha.uy@rapha.uy>
2024-11-28 15:56:59 -0300

    CRM sheet details

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -16,7 +16,7 @@ const badgeVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80 border-gray-300",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
-        outline: "text-foreground",
+        outline: "text-foreground justify-center",
         open: "bg-green-500 text-white rounded-2xl border-green-800",
         close: "bg-red-500 text-white rounded-2xl border-red-800",
         connecting: "bg-yellow-500 text-white rounded-2xl border-yellow-800",
