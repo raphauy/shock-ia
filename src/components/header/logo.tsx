dab10ffb05ac215c265f62d02d52ba176e41f093
raphauy <rapha.uy@rapha.uy>
2024-12-07 10:23:19 -0300

    Xplora logo

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -9,7 +9,8 @@ export default function Logo() {
   return (
     <Link href="/">
       <div className="text-3xl font-bold">
-        <Image src="/logo.png" width={200} height={50} alt="Shock logo" className="rounded-md" />
+        {/* <Image src="/logo.png" width={200} height={50} alt="Shock logo" className="rounded-md" /> */}
+        <Image src="/xplora.jpg" width={40} height={40} alt="Xplora logo" className="rounded-md" />
       </div>
     </Link>
 
