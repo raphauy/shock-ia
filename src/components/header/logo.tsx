854f8ef54a01a387c9eb5ae32eacaa4b0e627374
raphauy <rapha.uy@rapha.uy>
2024-12-09 09:40:41 -0300

    Shock logo

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -9,8 +9,7 @@ export default function Logo() {
   return (
     <Link href="/">
       <div className="text-3xl font-bold mt-1">
-        {/* <Image src="/logo.png" width={200} height={50} alt="Shock logo" className="rounded-md" /> */}
-        <Image src="/xplora.jpg" width={40} height={40} alt="Xplora logo" className="rounded-md" />
+        <Image src="/logo.png" width={200} height={50} alt="Shock logo" className="rounded-md" />
       </div>
     </Link>
 
