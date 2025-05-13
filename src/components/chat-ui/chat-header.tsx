dff1455b9a86c77368e85d3974c35546793e1cc5
raphauy <rapha.uy@rapha.uy>
2025-05-13 11:32:44 -0300

    Simulator Pro small UI fix

--- src/components/chat-ui/chat-header.tsx
+++ src/components/chat-ui/chat-header.tsx
@@ -21,24 +21,6 @@ function PureChatHeader({
   return (
     <header className="flex sticky top-0 bg-background py-1.5 items-center px-2 md:px-2 gap-2">
 
-      {(windowWidth < 768) && (
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="outline"
-              className="order-2 md:order-1 md:px-2 px-2 md:h-fit ml-auto md:ml-0"
-              onClick={() => {
-                router.push('/');
-                router.refresh();
-              }}
-            >
-              <PlusIcon />
-              <span className="md:sr-only">Nuevo Chat</span>
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent>Nuevo Chat</TooltipContent>
-        </Tooltip>
-      )}
 
       <ModelSelector
         selectedModelId={selectedModelId}
